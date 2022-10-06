22a7e4f241a5aa69a5e7a0627e50764e44dddbdd
eleonora <nebrudko18@gmail.com>
2022-10-06 12:09:43 +0300

    adding dist subtree

--- dist/js/160.878b61fe.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkbig_project"]=self["webpackChunkbig_project"]||[]).push([[160],{7160:function(n,e,c){c.r(e),c.d(e,{default:function(){return a}});var t=c(3396);const s={class:"div"};function o(n,e,c,o,r,i){return(0,t.wg)(),(0,t.iD)("div",s,"This is async component!")}var r={name:"AsyncComponent",components:{}},i=c(89);const u=(0,i.Z)(r,[["render",o]]);var a=u}}]);
-//# sourceMappingURL=160.878b61fe.js.map
---
+++ dist/js/225.8bc382d2.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkbig_project"]=self["webpackChunkbig_project"]||[]).push([[225],{4225:function(n,e,c){c.r(e),c.d(e,{default:function(){return a}});var t=c(3396);const s={class:"div"};function o(n,e,c,o,r,i){return(0,t.wg)(),(0,t.iD)("div",s,"This is async component!")}var r={name:"AsyncComponent",components:{}},i=c(89);const u=(0,i.Z)(r,[["render",o]]);var a=u}}]);
+//# sourceMappingURL=225.8bc382d2.js.map
